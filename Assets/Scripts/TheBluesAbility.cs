9b6c25702045cf49d4e0ec8e71f35cafaf61c72c
Mohamad Moheb <mohamadmoheb7@gmail.com>
2021-10-17 17:06:28 +0300

    Update TheBluesAbility.cs

--- Assets/Scripts/TheBluesAbility.cs
+++ Assets/Scripts/TheBluesAbility.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class TheBluesAbility : MonoBehaviour
 {
+
+    public bool _activated;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,15 @@ public class TheBluesAbility : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && GetComponent<Rigidbody2D>().velocity.magnitude > 2 && _activated == false)
+        {
+            _activated = true;
+            Debug.Log("Ability Activated");
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            _activated = false;
+        }
     }
 }
