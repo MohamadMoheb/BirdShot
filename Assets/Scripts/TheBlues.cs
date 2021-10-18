1a68e5c504bdc9b207ceac4aea59a51e3cf5eaee
Mohamad Moheb <mohamadmoheb7@gmail.com>
2021-10-18 16:57:26 +0300

    Update TheBlues.cs

--- Assets/Scripts/TheBlues.cs
+++ Assets/Scripts/TheBlues.cs
@@ -7,9 +7,10 @@ public class TheBlues : MonoBehaviour
     [SerializeField] private float _forceMuliplier = 250F;
     [SerializeField] private Vector3 _initialPosition;
 
+    public Rigidbody2D rb; //reference rigidbody2d
     void Start()
     {
-        GetComponent<Rigidbody2D>().gravityScale = 0;
+        rb.gravityScale = 0;
     }
 
     void Awake()
@@ -22,14 +23,14 @@ public class TheBlues : MonoBehaviour
         GetComponent<LineRenderer>().SetPosition(0, transform.position);    //Sets Line Renderer Extreme 1 on initial bird position
         GetComponent<LineRenderer>().SetPosition(1, _initialPosition);      //Sets Line Renderer Extreme 2 on current bird position
 
-        if (Input.GetKeyDown(KeyCode.R))                                    //Resets Script
+        if (Input.GetKeyDown(KeyCode.R))                //Reset Script
         {
-            GetComponent<Rigidbody2D>().gravityScale = 0;                   //Turns Gravity Off
-            transform.position = _initialPosition;                          //Resets Position
-            transform.rotation = Quaternion.identity;                       //Resets Rotation
-            GetComponent<Rigidbody2D>().angularVelocity = 0;                //Stops Rotation Force
-            GetComponent<Rigidbody2D>().velocity = Vector3.zero;            //Stops Movement Force
-            Debug.Log("position reset");                                    //Logs In Console
+            rb.gravityScale = 0;                        //Turns Gravity Off
+            transform.position = _initialPosition;      //Resets Position
+            transform.rotation = Quaternion.identity;   //Resets Rotation
+            rb.angularVelocity = 0;                     //Stops Rotation Force
+            rb.velocity = Vector3.zero;                 //Stops Movement Force
+            Debug.Log("position reset");                //Logs In Console
         }
     }
 
