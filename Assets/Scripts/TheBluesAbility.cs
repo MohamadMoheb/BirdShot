af9bd60c3b981cf2e451c12dfc9e2b034ad5d2e0
Mohamad Moheb <mohamadmoheb7@gmail.com>
2021-10-18 16:57:28 +0300

    Update TheBluesAbility.cs

--- Assets/Scripts/TheBluesAbility.cs
+++ Assets/Scripts/TheBluesAbility.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class TheBluesAbility : MonoBehaviour
 {
     [SerializeField] private GameObject prefab;
-    [SerializeField] private Vector3 _spawnpoint1edit;
-    [SerializeField] private Vector3 _spawnpoint2edit;
-    [SerializeField] private Vector3 _spawnpoint1;
-    [SerializeField] private Vector3 _spawnpoint2;
+    [SerializeField] private Vector3 _spawnpoint1edit;//spawn location editor for clone 1
+    [SerializeField] private Vector3 _spawnpoint2edit;//spawn location editor for clone 2
+    [SerializeField] private Vector3 _spawnpoint1; // set spawn point for clone 1
+    [SerializeField] private Vector3 _spawnpoint2; //set spawn point for clone 2
 
-    
     public bool _activated;
 
     // Update is called once per frame
@@ -35,5 +34,7 @@ public class TheBluesAbility : MonoBehaviour
         {
             _activated = false;
         }
+
+        Debug.Log("rotation"+ transform.rotation);
     }
 }
