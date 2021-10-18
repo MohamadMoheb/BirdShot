cfe35dc863856c16223bc3698f55be205545a7bb
Mohamad Moheb <mohamadmoheb7@gmail.com>
2021-10-18 16:26:56 +0300

    Update TheBluesAbility.cs

--- Assets/Scripts/TheBluesAbility.cs
+++ Assets/Scripts/TheBluesAbility.cs
@@ -5,37 +5,30 @@ using UnityEngine;
 public class TheBluesAbility : MonoBehaviour
 {
     [SerializeField] private GameObject prefab;
-    [SerializeField] private Vector2 spawnPosition;
-    [SerializeField] private bool random;
+    [SerializeField] private Vector3 _spawnpoint1edit;
+    [SerializeField] private Vector3 _spawnpoint2edit;
+    [SerializeField] private Vector3 _spawnpoint1;
+    [SerializeField] private Vector3 _spawnpoint2;
+
     
     public bool _activated;
 
-    // Start is called before the first frame update
-    /*public void OnSpawnPrefab()
-    {
-        if(random)
-        {
-            float x = Random.Range(-8,8);
-            float y = Random.Range(-4,4);
-            Instantiate(prefab, new Vector2(x, y), Quaternion.identity);
-        }
-        else
-        {
-            Instantiate(prefab, spawnPosition, Quaternion.identity);
-        }
-    }
-*/
     // Update is called once per frame
     void Update()
     {
+        _spawnpoint1edit.Set(0,+2,0);
+        _spawnpoint2edit.Set(0,-2,0);
+
         if (Input.GetKeyDown(KeyCode.Space) && GetComponent<Rigidbody2D>().velocity.magnitude > 2 && _activated == false)
         {
             _activated = true;
             Debug.Log("Ability Activated");
 
-            float x = Random.Range(-8,8);
-            float y = Random.Range(-4,4);
-            Instantiate(prefab, new Vector2(x, y), Quaternion.identity);
+            _spawnpoint1 = transform.position + _spawnpoint1edit;
+            _spawnpoint2 = transform.position + _spawnpoint2edit;
+
+            Instantiate(prefab, _spawnpoint1, Quaternion.identity);
+            Instantiate(prefab, _spawnpoint2, Quaternion.identity);
         }
 
         if (Input.GetKeyDown(KeyCode.R))
