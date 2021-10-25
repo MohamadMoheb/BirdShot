4fddb6a1818fffde424211d875a7749609f0373a
Mohamad Moheb <mohamadmoheb7@gmail.com>
2021-10-25 22:10:20 +0300

    Update TheBluesAbility.cs

--- Assets/Scripts/TheBluesAbility.cs
+++ Assets/Scripts/TheBluesAbility.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TheBluesAbility : MonoBehaviour
 {
-    float offset = 1;
+    float offset = 2;
     public GameObject Player;
 
     public Rigidbody2D rb; //reference rigidbody2d
@@ -13,26 +13,16 @@ public class TheBluesAbility : MonoBehaviour
     private Vector3 _spawnpoint1edit;//spawn location editor for clone 1
     private Vector3 _spawnpoint2edit;//spawn location editor for clone 2
     private Vector3 _spawnpoint3edit;//spawn location editor for clone 3
-    [SerializeField] private Vector3 _spawnpoint1; // set spawn point for clone 1
-    [SerializeField] private Vector3 _spawnpoint2; //set spawn point for clone 2
-    [SerializeField] private Vector3 _spawnpoint3; //set spawn point for clone 3
 
     public bool _activated;
 
     void Update()
     {
-        _spawnpoint1edit.Set(0,+2,0);
-        _spawnpoint2edit.Set(0,0,0);
-        _spawnpoint3edit.Set(0,-2,0);
 
         if (Input.GetKeyDown(KeyCode.Space) && GetComponent<Rigidbody2D>().velocity.magnitude > 2 && _activated == false)
         {
             _activated = true;
             Debug.Log("Ability Activated");
-
-            _spawnpoint1 = transform.position + _spawnpoint1edit;
-            _spawnpoint2 = transform.position + _spawnpoint2edit;
-            _spawnpoint3 = transform.position + _spawnpoint3edit;
 
             GameObject clone1 = Instantiate(_prefab, transform.position + Vector3.up * offset, transform.rotation);
             GameObject clone2 = Instantiate(_prefab, transform.position, transform.rotation);
