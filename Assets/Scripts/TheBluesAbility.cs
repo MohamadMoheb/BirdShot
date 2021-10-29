57183c460b4d1865ed492c42f67b6a51d7b7e434
Mohamad Moheb <mohamadmoheb7@gmail.com>
2021-10-29 12:52:34 +0300

    Update TheBluesAbility.cs

--- Assets/Scripts/TheBluesAbility.cs
+++ Assets/Scripts/TheBluesAbility.cs
@@ -4,46 +4,14 @@ using UnityEngine;
 
 public class TheBluesAbility : MonoBehaviour
 {
-    float offset = 2;
-    public GameObject Player;
-
-    public Rigidbody2D rb; //reference rigidbody2d
-
-    [SerializeField] private GameObject _prefab;
-    private Vector3 _spawnpoint1edit;//spawn location editor for clone 1
-    private Vector3 _spawnpoint2edit;//spawn location editor for clone 2
-    private Vector3 _spawnpoint3edit;//spawn location editor for clone 3
-
-    public bool _activated;
-
-    public Vector2 _velocitytest;
+    [SerializeField] private GameObject _clones;
 
     void Update()
     {
-
-        _velocitytest = GetComponent<Rigidbody2D>().velocity;
-
-        if (Input.GetKeyDown(KeyCode.Space) && GetComponent<Rigidbody2D>().velocity.magnitude > 2 && _activated == false)
+        if (Input.GetKeyDown(KeyCode.R))                //Reset Script
         {
-            _activated = true;
-            Debug.Log("Ability Activated");
-
-            GameObject clone1 = Instantiate(_prefab, transform.position + Vector3.up * offset, transform.rotation);
-            GameObject clone2 = Instantiate(_prefab, transform.position, transform.rotation);
-            GameObject clone3 = Instantiate(_prefab, transform.position + Vector3.up * offset * -1, transform.rotation);
-
-            //clone1.GetComponent<Rigidbody2D>().velocity = rb.velocity;
-            //clone2.GetComponent<Rigidbody2D>().velocity = rb.velocity;
-            //clone3.GetComponent<Rigidbody2D>().velocity = rb.velocity;
-
-            gameObject.SetActive(false);
-        }
-
-        print("rbvelo"+ _velocitytest);
-
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            _activated = false;
+            _clones.SetActive(false);
+            Debug.Log("position reset (2)");            //Logs In Console
         }
     }
 }
