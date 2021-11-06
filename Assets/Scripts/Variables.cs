b01d42f9af51673eb50ea7c7fe4b04b836184617
Mohamad Moheb <mohamadmoheb7@gmail.com>
2021-11-06 05:35:32 +0200

    Create Variables.cs

---
+++ Assets/Scripts/Variables.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    public Vector2 _BlueVelocity;
+    public Vector2 _JayVelocity;
+    public Vector2 _JakeVelocity;
+    public Vector2 _JimVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
